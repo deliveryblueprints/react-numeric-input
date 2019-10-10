310896a45ff7bf444e4472bfe43d565f5a1cc3ad
emmanouile <manos.emmanouil@deliveryblueprints.com>
2019-10-10 12:55:49 +0100

    fix: BAP-283 call ok change even when state value is empty string

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ module.exports =
 	    }, {
 	        key: 'componentDidUpdate',
 	        value: function componentDidUpdate(prevProps, prevState) {
-	            if (prevState.value !== this.state.value && (this.state.value !== "" || this.state.value === null)) {
+	            if (prevState.value !== this.state.value && this.state.value === null) {
 	                    this._invokeEventCallback("onChange", this.state.value, this.refsInput.value, this.refsInput);
 	                }
 
